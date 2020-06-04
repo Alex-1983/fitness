a84f2825283a1901eb33829ae87058acc576fb15
Alex-1983 <39596822+Alex-1983@users.noreply.github.com>
2020-06-04 14:45:40 +0500

    добавляет правки а абонементы

--- build/js/main.js
+++ build/js/main.js
@@ -1,19 +1,20 @@
 'use strict';
 
-(function() {
+(function () {
 
-  var tabs = document.querySelectorAll(".tabs__item");
-  var cards = document.querySelectorAll(".cards");
-  var activateTabClassName = "tabs__item--active";
-  var activateCardsClassName = "cards--active";
+  var tabs = document.querySelectorAll('.tabs__item');
+  var cards = document.querySelectorAll('.cards');
+  var activateTabClassName = 'tabs__item--active';
+  var activateCardsClassName = 'cards--active';
 
-  var onTabItemClick = function(evt) {
+  var onTabItemClick = function (evt) {
     evt.preventDefault();
     for (var i = 0; i < tabs.length; i++) {
       tabs[i].classList.remove(activateTabClassName);
-    };
+    }
+
     for (var j = 0; j < tabs.length; j++) {
-      if (tabs[j] == this) {
+      if (tabs[j] === this) {
         tabs[j].classList.add(activateTabClassName);
         for (var h = 0; h < cards.length; h++) {
           cards[h].classList.remove(activateCardsClassName);
@@ -24,8 +25,7 @@
   };
 
   for (var i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", onTabItemClick)
+    tabs[i].addEventListener('click', onTabItemClick);
   }
 
 })();
-
