c511ff35f8469009a6153134cde225a35bd7864a
Alex-1983 <39596822+Alex-1983@users.noreply.github.com>
2020-06-05 18:10:18 +0500

    добавляет правки к код-ревью

--- build/js/main.js
+++ build/js/main.js
@@ -2,30 +2,75 @@
 
 (function () {
 
+  // Табы
+  var PeriodSessions = {
+    ONE_MONTH: '1 месяц',
+    SIX_MONTH: '6 месяцев',
+    TWELVE_MONTH: '12 месяцев'
+  };
+
+  var PriceSessions = {
+    ONE_MONTH: [5000, 1700, 2700],
+    SIX_MONTH: [27000, 9000, 14000],
+    TWELVE_MONTH: [51000, 17000, 27000]
+  };
+
+  var NumberSessions = {
+    ONE_MONTH: '12 занятий',
+    SIX_MONTH: '72 занятия',
+    TWELVE_MONTH: '144 занятия'
+  };
+
   var tabs = document.querySelectorAll('.tabs__item');
-  var cards = document.querySelectorAll('.cards');
+  var numberSessions = document.querySelector('.cards__number');
+  var costSessions = document.querySelectorAll('.cards__price');
+  var costSessionsBig = document.querySelectorAll('.cards__price-big');
   var activateTabClassName = 'tabs__item--active';
-  var activateCardsClassName = 'cards--active';
 
-  var onTabItemClick = function (evt) {
-    evt.preventDefault();
+
+  var fillNumberSessions = function (value) {
+    numberSessions.textContent = value;
+  };
+
+  var fillPriceSessions = function (el, price) {
     for (var i = 0; i < tabs.length; i++) {
-      tabs[i].classList.remove(activateTabClassName);
+      el[i].textContent = price[i];
     }
+  };
 
-    for (var j = 0; j < tabs.length; j++) {
-      if (tabs[j] === this) {
-        tabs[j].classList.add(activateTabClassName);
-        for (var h = 0; h < cards.length; h++) {
-          cards[h].classList.remove(activateCardsClassName);
+  var onTabClick = function () {
+    for (var i = 0; i < tabs.length; i++) {
+      var tab = tabs[i];
+      var tabContent = tab.textContent;
+
+      tab.classList.remove(activateTabClassName);
+
+      if (tab === this) {
+        tab.classList.add(activateTabClassName);
+
+        if (tabContent === PeriodSessions.ONE_MONTH) {
+          fillNumberSessions(NumberSessions.ONE_MONTH);
+          fillPriceSessions(costSessions, PriceSessions.ONE_MONTH);
+          fillPriceSessions(costSessionsBig, PriceSessions.ONE_MONTH);
         }
-        cards[j].classList.add(activateCardsClassName);
+
+        if (tabContent === PeriodSessions.SIX_MONTH) {
+          fillNumberSessions(NumberSessions.SIX_MONTH);
+          fillPriceSessions(costSessions, PriceSessions.SIX_MONTH);
+          fillPriceSessions(costSessionsBig, PriceSessions.SIX_MONTH);
+        }
+
+        if (tabContent === PeriodSessions.TWELVE_MONTH) {
+          fillNumberSessions(NumberSessions.TWELVE_MONTH);
+          fillPriceSessions(costSessions, PriceSessions.TWELVE_MONTH);
+          fillPriceSessions(costSessionsBig, PriceSessions.TWELVE_MONTH);
+        }
       }
     }
   };
 
   for (var i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', onTabItemClick);
+    tabs[i].addEventListener('click', onTabClick);
   }
 
 })();
